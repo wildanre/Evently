c645658af4e13b170431a937d9f8c7666affd460
FjrREPO <jossfajar21@gmail.com>
2025-06-13 18:35:28 +0700

    feat: add mobile and desktop navbar components

---
+++ evently-frontend/src/components/navbar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const MobileNavbar = () => {
+  return (
+    <div className="flex lg:hidden w-full">
+      <div className="text-lg font-bold">Logo</div>
+      <nav className="space-x-4">
+        <a href="#" className="">Home</a>
+        <a href="#" className="">About</a>
+        <a href="#" className="">Services</a>
+        <a href="#" className="">Contact</a>
+      </nav>
+    </div>
+  )
+}
+
+const DesktopNavbar = () => {
+  return (
+    <div className="hidden lg:flex w-full">
+      <div className="text-lg font-bold">Logo</div>
+      <nav className="space-x-4">
+        <a href="#" className="">Home</a>
+        <a href="#" className="">About</a>
+        <a href="#" className="">Services</a>
+        <a href="#" className="">Contact</a>
+      </nav>
+    </div>
+  )
+}
+
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      <MobileNavbar />
+      <DesktopNavbar />
+    </React.Fragment>
+  )
+}
+
+export { Navbar };
